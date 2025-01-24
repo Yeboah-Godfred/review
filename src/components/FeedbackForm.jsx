678471f83be8c2a73412dcef797017e7d3886933
Yeboah-Godfred <ygodfred3@gmail.com>
2025-01-24 16:52:16 +0000

    Form validation is FeedbackForm component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,8 +6,22 @@ import Button from '../shared/Button'
 function FeedbackForm() {
 
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+
+        if(message === ""){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if(text !== "" && text.trim().length <= 10){
+            setBtnDisabled(true)
+            setMessage("Message should be at least 10 characters")
+        }else{
+            setBtnDisabled(false)
+            setMessage(null)
+        }
+
         setText(e.target.value)
     }
 
@@ -17,10 +31,11 @@ function FeedbackForm() {
         <h2>Rate your service with us</h2>
         <div className="input-group">
             <input onChange={handleTextChange} type='text' placeholder='Write a Review' value={text}></input>
-            <Button type='submit' version='primary' >
+            <Button type='submit' version='primary' isDisabled={btnDisabled}>
                 Submit
             </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
     </form>
    </Card>
   )
