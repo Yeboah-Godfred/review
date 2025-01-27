4bcc34cca4139c725d62f8133586b022a15d4ba4
Yeboah-Godfred <ygodfred3@gmail.com>
2025-01-27 12:38:15 +0000

    Routing which needs fixing due to the version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 
 function App() {
@@ -23,14 +25,21 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header heading="Feedback & Ratings"/>
       <div className="container-app">
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <Routes>
+           <Route exact path="/">
+            <FeedbackForm handleAdd={addFeedback}/>     
+            <FeedbackStats feedback={feedback}/>
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+          </Route> 
+        
+          <Route path="/about" Component={AboutPage}/>
+        </Routes>
+
       </div>
-    </>
+    </Router>
     
   );
 }
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../shared/Card'
+
+function AboutPage() {
+  return <Card>
+    <div className="about">
+        <h3>About this app</h3>
+        <p>This app was created to give feedback about our sold products</p>
+        <p>Version: 1.0.0</p>
+    </div>
+  </Card>
+}
+export default AboutPage
